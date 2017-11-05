f639c0cecac369866243432e2bec7309b3ea9c05
ikostan <superegorius@gmail.com>
2017-11-04 18:18:46 -0700

    Problem 4: Iterate over each element in d_list and print it using the for loop.

--- ArrayAndList/ArrayAndListProgram.cs
+++ ArrayAndList/ArrayAndListProgram.cs
@@ -13,17 +13,20 @@ namespace ArrayAndList
 
             //Problem 1: Initialize a new list called a_list with the elements ["item1", "item2", "item3"]
             a_list = new string[] { "item 1", "item 2", "item 3" };
-            RunByIndex(a_list);
-            RunAsList(a_list);
+            //RunByIndex(a_list);
+            //RunAsList(a_list);
 
             //Problem 2: Change the second element in b_list(defined below) to 1.
             b_list = new int[] { 0, 0, 0 };
             ChangeElement(b_list, 1, 1);
-            RunAsList(b_list);
+            //RunAsList(b_list);
 
             //Problem 3: Iterate over each element in c_list and print it using the foreach loop.
             c_list = new string[] { "item0", "item1", "item2" };
-            RunAsList(c_list);
+            //RunAsList(c_list);
+
+            //Problem 4: Iterate over each element in d_list and print it using the for loop.
+            RunByIndex(b_list);
 
             Console.ReadKey();
         }
@@ -33,6 +36,18 @@ namespace ArrayAndList
         /// </summary>
         /// <param name="arr"></param>
         private static void RunByIndex(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        /// <summary>
+        /// Display array members using index
+        /// </summary>
+        /// <param name="arr"></param>
+        private static void RunByIndex(int[] arr)
         {
             for (int i = 0; i < arr.Length; i++)
             {
