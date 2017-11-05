a37dad3718e49bf3762801d4f713aca9d67d2e5d
ikostan <superegorius@gmail.com>
2017-11-05 00:37:09 -0700

    Comments added

---
+++ TwoDArray/TwoDArrayProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TwoDArray
+{
+    class TwoDArrayProgram
+    {
+        /// <summary>
+        /// 
+        /// - [Narrator] Here's a quick overview of two-dimensional array. 
+        /// Two dimensional arrays are often used in coding interviews, so let's examine how it works. 
+        /// Conceptually, a two-dimensional array is a collection of items, for example, a collection of numbers, 
+        /// that's laid out in a two-dimensional table. For example, this table on the screen has two rows and four 
+        /// columns with the items, one, three, nine, four, five, zero, eight, and negative three. 
+        /// A two-dimensional array like this one is usually implemented as an array of arrays.
+        ///
+        /// So this two-dimensional array would be typically represented as an array of two arrays, 
+        /// the first one containing the first row, one, three, nine, four, and the second array containing the second row, 
+        /// five, zero, eight, and negative three.One common assumption here is that a two-dimensional array has a rectangular shape. 
+        /// What that means is, each row has the same number of items as all the other rows. You might want to clarify this by asking 
+        /// something like, "can we assume that each row has the same number of items?" 
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
