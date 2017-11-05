936d0094373d6dd20461bbfc8490bab2d56a424f
ikostan <superegorius@gmail.com>
2017-11-04 17:38:35 -0700

    Add project files.

---
+++ GetReadyForCodingInterview/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetReadyForCodingInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
