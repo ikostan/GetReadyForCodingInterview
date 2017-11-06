0dfcdcd6d29f7a6f5a0c5b714e559437fea38192
ikostan <superegorius@gmail.com>
2017-11-05 21:47:21 -0800

    Internal solution structure changed

--- ArrayAndList/ArraysProgram.cs
+++ ArrayAndList/ArraysProgram.cs
@@ -10,19 +10,21 @@ namespace ArrayAndList
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Arrays:\n");
+
             //Problem 1: Initialize a new list called a_list with the elements ["item1", "item2", "item3"]
             a_list = new string[] { "item 1", "item 2", "item 3" };
-            //RunByIndex(a_list);
+            RunByIndex(a_list);
             //RunAsList(a_list);
 
             //Problem 2: Change the second element in b_list(defined below) to 1.
             b_list = new int[] { 0, 0, 0 };
             ChangeElement(b_list, 1, 1);
-            //RunAsList(b_list);
+            RunAsList(b_list);
 
             //Problem 3: Iterate over each element in c_list and print it using the foreach loop.
             c_list = new string[] { "item0", "item1", "item2" };
-            //RunAsList(c_list);
+            RunAsList(c_list);
 
             //Problem 4: Iterate over each element in d_list and print it using the for loop.
             RunByIndex(b_list);
