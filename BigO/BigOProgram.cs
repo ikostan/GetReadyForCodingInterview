a68240865c16e700f758fe3ad3ba07cc1b5bc854
ikostan <superegorius@gmail.com>
2017-11-05 21:29:36 -0800

    Big O project created

---
+++ BigO/BigOProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BigO
+{
+    class BigOProgram
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time Complexity and Big O:\n");
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
