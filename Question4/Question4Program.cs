abb58cd5440a6d4ea280902d5a1942af34acac1b
ikostan <superegorius@gmail.com>
2017-11-06 21:14:32 -0800

    First run

--- Question4/Question4Program.cs
+++ Question4/Question4Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Question4
 {
@@ -13,6 +14,11 @@ namespace Question4
         /// If you look at each row from left to right the integers always increase or stay the same, 
         /// and I want you to write a function that takes this 2D array as the input and returns the number of negative numbers in it.
         /// 
+        /// -4 -3 -1  1
+        /// -2 -2  1  2
+        /// -1  1  2  3
+        ///  1  2  4  5
+        /// 
         /// So, for instance, for this array we should return six because there are six negative numbers.
         /// First, try finding a big O of N squared solution.Then, try finding a more efficient solution with 
         /// linear time complexity or big O of N. For clarifying questions you might ask is the 2D array always square shaped? 
@@ -24,9 +30,90 @@ namespace Question4
         /// </summary>
         /// <param name="args"></param>
 
+        private static int[,] arr2D;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Question #4:\n");
+
+            arr2D = new int[4, 4] {
+                { -4, -3, -1, 1 },
+                { -2, -2,  1, 2 },
+                { -1,  1,  2, 3 },
+                {  1,  2,  4, 5 }
+            };
+
+            Console.WriteLine($"O(n2): {count_negatives_On2(arr2D)}");
+
+            Console.WriteLine($"O(n): {count_negatives_On(arr2D)}");
+
+            Console.ReadKey();
         }
+
+
+        /// <summary>
+        /// Function that takes this 2D array as the input and returns the number of negative numbers in it.
+        /// Big O: O(n2)
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        private static int count_negatives_On2(int[,] arr)
+        {
+            int negatives = 0;
+            int times = 0;
+
+            foreach (var item in arr)
+            {
+                if (item < 0)
+                {
+                    negatives++;
+                }
+
+                times++;
+            }
+
+            Debug.WriteLine($"Times runned: {times}");
+
+            return negatives;
+        }
+
+        /// <summary>
+        /// Function that takes this 2D array as the input and returns the number of negative numbers in it.
+        /// Big O: O(n)
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        private static int count_negatives_On(int[,] arr)
+        {
+            int negatives = 0;
+            int times = 0;
+
+            for (int row = 0; row < arr.GetLength(0); row++)
+            {
+                if (arr[row, 0] > -1)
+                {
+                    continue;
+                }
+
+                for (int col = 0; col < arr.GetLength(0); col++)
+                {
+                    times++;
+
+                    if (arr[row, col] > -1)
+                    {
+                        break;
+                    }
+                    else 
+                    {
+                        negatives++;
+                    }
+                }
+            }
+
+            Debug.WriteLine($"Times runned: {times}");
+
+            return negatives;
+        }
+
     }
 }
