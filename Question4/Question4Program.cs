3b7ca8ab0a0cbd2fb11ec21f2b41a72006fcff6d
ikostan <superegorius@gmail.com>
2017-11-06 20:52:33 -0800

    Question #4 added

---
+++ Question4/Question4Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Question4
+{
+    class Questin4Program
+    {
+        /// <summary>
+        /// 
+        /// - [Instructor] Here is sample interview question number four with two dimensional array and time complexity. 
+        /// Here's the problem. You're given a two dimensional array with integers which is square shaped which means 
+        /// that the number of rules is equal to the number of columns, and this array has a special property that each 
+        /// row and each column is sorted. So, if you look at each column from top to bottom, the integers always increase or stay the same. 
+        /// If you look at each row from left to right the integers always increase or stay the same, 
+        /// and I want you to write a function that takes this 2D array as the input and returns the number of negative numbers in it.
+        /// 
+        /// So, for instance, for this array we should return six because there are six negative numbers.
+        /// First, try finding a big O of N squared solution.Then, try finding a more efficient solution with 
+        /// linear time complexity or big O of N. For clarifying questions you might ask is the 2D array always square shaped? 
+        /// Yes, as I said earlier, the number of columns is equal to the number of rows. What about zero? Zero 
+        /// is not counted as a negative number.
+        /// 
+        /// As usual, try solving this problem and make sure to run your code.
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
