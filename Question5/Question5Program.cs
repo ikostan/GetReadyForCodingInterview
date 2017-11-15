02f5e34b20d07c1ba7e89df027756559d1530d9e
ikostan <superegorius@gmail.com>
2017-11-14 22:01:11 -0800

    O(n) method added

--- Question5/Question5Program.cs
+++ Question5/Question5Program.cs
@@ -18,7 +18,7 @@ namespace Question5
         /// Could the numbers be negative? Yes, any of the numbers in the array could be negative or zero.
         /// What if I find two or more pairs that add up to ten? Then just print out one of them, and you can choose which one.
         /// What if there's no pair that adds up to ten? Then just print there's no pair that adds 
-        /// up to ten and return from the function. 
+        /// up to ten and return from the function, and trying solving this problem in O(n).  
         /// Once you solve it, make sure to run a code to check if it's correct.
         /// 
         /// </summary>
@@ -35,23 +35,28 @@ namespace Question5
 
             // [3, 4, 1, 2, 9] => (Should print 1, 9)
             int[] arr1 = new int[] { 3, 4, 1, 2, 9 };
-            pair10(arr1);
+            //pair10(arr1);
+            pair10_On(arr1);
 
             // [-11, -20, 2, 4, 30] => (Should print -20, 30)
             int[] arr2 = new int[] { -11, -20, 2, 4, 30 };
-            pair10(arr2);
+            //pair10(arr2);
+            pair10_On(arr2);
 
             // [1, 2, 9, 8] => (Should print 1, 9 or 2, 8)
             int[] arr3 = new int[] { 1, 2, 9, 8 };
-            pair10(arr3);
+            //pair10(arr3);
+            pair10_On(arr3);
 
             // [1, 1, 1, 2, 3, 9] => (Should print 1, 9)
             int[] arr4 = new int[] { 1, 1, 1, 2, 3, 9 };
-            pair10(arr4);
+            //pair10(arr4);
+            pair10_On(arr4);
 
             // [1, 1, 1, 2, 3, 4, 5] => (Should print "There is no pair that adds up to 10.)
             int[] arr5 = new int[] { 1, 1, 1, 2, 3, 4, 5 };
-            pair10(arr5);
+            //pair10(arr5);
+            pair10_On(arr5);
 
             Console.ReadKey();
         }
@@ -67,7 +72,7 @@ namespace Question5
 
             for (int i = 0; i < arr.Length - 1; i++)
             {
-                if (!numPairs.ContainsKey(arr[i]))
+                if (!numPairs.ContainsKey(arr[i])) //Optimization
                 {
                     for (int b = i + 1; b < arr.Length; b++)
                     {
@@ -93,9 +98,37 @@ namespace Question5
             }
         }
 
+        /// <summary>
+        /// Big O of n method
+        /// </summary>
+        /// <param name="arr"></param>
         private static void pair10_On(int[] arr)
         {
+            Hashtable testedNumbers = new Hashtable();
+            bool pairsExists = false;
 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!testedNumbers.ContainsKey(arr[i])) //Check is Key exist => has to be done in order to avoid exceptions
+                {
+                    if (testedNumbers.ContainsKey(10 - arr[i])) //Pair found
+                    {
+                        Console.WriteLine($"{arr[i]}, {(10 - arr[i])}");
+                        pairsExists = true;
+                    }
+
+                    testedNumbers.Add(arr[i], 1);
+                }
+                else
+                {
+                    continue; //Skip to the next number
+                }
+            }
+
+            if (!pairsExists)
+            {
+                Console.WriteLine("There is no pair that adds up to 10.");
+            }
         }
 
     }
